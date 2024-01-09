3004a6129a10e6189b2367253a6c9830720df436
Megan Walford <mwalford22@gmail.com>
2024-01-09 22:04:16 +0000

    Testing JQuery

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+//$(document).ready(function() {
+  //  $("h2").css("text-decoration", "underline"); //underline all h2 elements
+  //  $("ul").css("border", "solid 1px #ccc"); //adds border to all lists
+//});
+
+$(document).ready(function() {
+    $("h2").addClass("underline"); //underline all h2 elements
+    $("ul").addClass("border"); //adds border to all lists
+});
+
+//$(document).ready(function() {
+  //  $("h2").removeClass("underline"); //underline all h2 elements
+  //  $("ul").removeClass("border"); //adds border to all lists
+//});
